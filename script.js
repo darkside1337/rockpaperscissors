b22f669f679c59b2185ec4c42ab98b6ce6c472fc
darkside1337 <medini.ali.2000@gmail.com>
2023-08-19 00:44:50 +0100

    finished

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let playerChoice = "scissors";
-let computerSelection = getComputerChoice();
 let computerScore = 0;
 let playerScore = 0;
-// ** //
+const scoreBoard = document.querySelector("#scoreBoard");
+const currentScore = document.querySelector("#currentScore");
+const finalResult = document.querySelector("#finalResult");
+const computerScoreBoard = document.querySelector("#computerScoreBoard");
+const playerScoreBoard = document.querySelector("#playerScoreBoard");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3) + 1;
   if (randomNum === 1) {
@@ -14,7 +20,8 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice();
   playerSelection = playerSelection.toLowerCase();
   if (playerSelection === "rock" && computerSelection === "rock") {
     return "You both choose rock, It's a draw!";
@@ -46,15 +53,54 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let result = playRound(prompt("Pick your weapon?"), computerSelection);
-    console.log(result);
+console.log(rock, paper, scissors);
+
+rock.addEventListener("click", () => {
+  const result = playRound("rock");
+  updateDivResult(result);
+  updateCurrentScore();
+  updateFinalScore();
+});
+paper.addEventListener("click", () => {
+  const result = playRound("paper");
+  updateDivResult(result);
+  updateCurrentScore();
+  updateFinalScore();
+});
+
+scissors.addEventListener("click", () => {
+  const result = playRound("scissors");
+  updateDivResult(result);
+  updateCurrentScore();
+  updateFinalScore();
+});
+
+function updateDivResult(text) {
+  scoreBoard.textContent = text;
+}
+function updateCurrentScore() {
+  computerScoreBoard.textContent = computerScore;
+  playerScoreBoard.textContent = playerScore;
+}
+function finalResultFunction(playerScore, computerScore) {
+  if (playerScore >= 5 || computerScore >= 5) {
+    if (playerScore > computerScore) {
+      disableBtn();
+      return "Congratulations, you win!";
+    } else if (computerScore > playerScore) {
+      disableBtn();
+      return "what a bummer, you loose!";
+    }
   }
-  if (playerScore > computerScore) {
-    return "Congratulations, you win!";
-  } else if (computerScore > playerScore) {
-    return "what a bummer, you loose!";
+  return;
+}
+function updateFinalScore() {
+  const result = finalResultFunction(playerScore, computerScore);
+  finalResult.textContent = result;
+}
+function disableBtn() {
+  const buttons = document.getElementsByClassName("btn");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
   }
 }
-game();
